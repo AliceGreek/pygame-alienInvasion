41a43666a1380585a828159409dbe20ba58a4810
Alice <1158636487@qq.com>
2018-01-28 21:58:41 +0800

    add bullet

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pygame-alienInvasion
+主文件：alien_invasion.py创建一系列整个游戏都要用到的对象:存储在ai_settings 中的设置、存储在screen 中的主显示surface以及一个飞船实例。文件alien_invasion.py还包含游 戏的主循环，这是一个调用check_events() 、ship.update() 和update_screen() 的while 循环。
+要玩游戏《外星人入侵》，只需运行文件alien_invasion.py。其他文件(settings.py、game_functions.py、ship.py)包含的代码被直接或间接地导入到这个文件中。
--- ship.py
+++ ship.py
@@ -16,22 +16,29 @@ class Ship():
 		self.rect.bottom = self.screen_rect.bottom
 
 		'''在飞船的属性center中存储小数值'''
-		self.center = float(self.rect.centerx)
+		self.center = [float(self.rect.centerx),float(self.rect.centery)]
 
 		'''移动标志'''
 		self.moving_right = False
 		self.moving_left = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		'''根据移动标志调整飞船位置'''
 		#更新飞船的center值，而不是rect值
 		if self.moving_right and self.rect.right < self.screen_rect.right:
-			self.center += self.ai_settings.ship_speed_factor
+			self.center[0] += self.ai_settings.ship_speed_factor
 		elif self.moving_left and self.rect.left > self.screen_rect.left:
-			self.center -= self.ai_settings.ship_speed_factor
+			self.center[0] -= self.ai_settings.ship_speed_factor
+		elif self.moving_up and self.rect.top > self.screen_rect.top:
+			self.center[1] -= self.ai_settings.ship_speed_factor
+		elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+			self.center[1] += self.ai_settings.ship_speed_factor
 
 		#根据self.center更新rect对象
-		self.rect.centerx = self.center
+		self.rect.centery = self.center[1]
+		self.rect.centerx = self.center[0]	
 			
 	def blitme(self):
 		'''在指定位置绘制飞船'''
